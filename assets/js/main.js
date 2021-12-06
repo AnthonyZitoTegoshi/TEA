a80fdf10969d27f112748ff362fd93cbe1254a86
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2021-12-05 23:15:39 -0300

    skwk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 function slide1(){
-    document.getElementById().src = "";
+    document.getElementById('slide').src = "assets\img\slideImg\1.jpg";
     setTimeout("slide2()", 1000);
 }
 function slide2(){
-    document.getElementById().src ="";
+    document.getElementById('slide').src ="assets\img\slideImg\2.jpg";
     setTimeout("slide3()",1000);
 }
 function slide3(){
-    document.getElementById().src = "";
+    document.getElementById('slide').src = "assets\img\slideImg\3.jpg";
     setTimeout("slide1()",1000);
 }
