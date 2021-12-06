42ea907e45d63c90b20dcf775ed34a342c3cddeb
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2021-12-05 23:09:23 -0300

    sq

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function slide1(){
+    document.getElementById().src = "";
+    setTimeout("slide2()", 1000);
+}
+function slide2(){
+    document.getElementById().src ="";
+    setTimeout("slide3()",1000);
+}
+function slide3(){
+    document.getElementById().src = "";
+    setTimeout("slide1()",1000);
+}
