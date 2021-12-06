07e1e4124fe0c5459a6b009007fa32d237c45d2a
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-06 09:37:07 -0300

    Logo responsiva

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,13 +30,13 @@ windowResized();
 
 function slide1(){
     document.getElementById('slide').src = "assets/img/slideImg/1.jpg";
-    setTimeout("slide2()", 1000);
+    setTimeout("slide2()", 3000);
 }
 function slide2(){
     document.getElementById('slide').src ="assets/img/slideImg/2.jpg";
-    setTimeout("slide3()",1000);
+    setTimeout("slide3()",3000);
 }
 function slide3(){
     document.getElementById('slide').src = "assets/img/slideImg/3.jpg";
-    setTimeout("slide1()", 1000);
+    setTimeout("slide1()", 3000);
 }
