e3e855028ef32b78ef4d02f7aef146f558fe11fb
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-14 15:54:41 -0300

    asd

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,7 +1,25 @@
 <?php
 
 class DBConnection {
+
+    private $host;
+    private $user;
+    private $password;
+    private $database;
+    private $connection;
+
+    /*function __construct($host, $user, $password, $database) {
+
+    }*/
     
+    public function getHost() {
+        return $this->host;
+    }
+
+    public function setHost($host) {
+        $this->host = $host;
+    }
+
 }
 
 ?>
--- sendus.php
+++ sendus.php
@@ -6,8 +6,9 @@ $userName = $_REQUEST["username"] ? $_REQUEST["username"] : null;
 $userContact = $_REQUEST["usercontact"] ? $_REQUEST["usercontact"] : null;
 $userMessage = $_REQUEST["usermessage"] ? $_REQUEST["usermessage"] : null;
 
-echo $userName . " e " . $userContact . " e " . $userMessage;
-
 $dbConnection = new DBConnection();
+
+$dbConnection->setHost("asd");
+echo $dbConnection->getHost();
 
 ?>
