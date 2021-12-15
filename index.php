b5257a4a5be86b8423c27e653ad86e735fd08563
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-15 17:58:21 -0300

    asd

--- index.php
+++ index.php
@@ -470,7 +470,7 @@
 
     <!-------------VERIFICAÇÃO_DE_STATUS_DA_MENSAGEM_DO_FORM------------->
     <?php
-        //session_start();
+        session_start(); echo $_SESSION["queryStatus"];
         if (isset($_SESSION["queryStatus"])) {
             echo "<div id='queryStatusPopup' class='container fit-child screen-center bg-dark-gradient dark padding-5 radius-15 lift-10'>
                 <div class='row-center items-center flex-nowrap flex-column padding-10 radius-10 bg-light'>
