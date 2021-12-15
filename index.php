18f91669099f47d692dac8a00e25d5348ab3c8ba
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-15 17:22:24 -0300

    asd

--- index.php
+++ index.php
@@ -470,7 +470,7 @@
 
     <div class="container fit-child screen-center bg-dark-gradient dark padding-5 radius-15 lift-10">
         <div class="row-center items-center flex-nowrap flex-column padding-10 radius-10 bg-light">
-            <div class="text-left font-size-md line-md poppins padding-10">
+            <div class="text-left line-md poppins padding-10">
                 Mensagem enviada com sucesso!
             </div>
             <div class="col fit-child padding-10">
