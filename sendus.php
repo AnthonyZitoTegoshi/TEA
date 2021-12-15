4389e247015d359c9eebe5b1021fba51cd9fc8e0
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-15 18:10:23 -0300

    asd

--- sendus.php
+++ sendus.php
@@ -24,5 +24,5 @@ $response = $dbConnection->insert("sendus", "id", 0, "username", $userName, "use
 // Volta para a página all-in-one enviando o resultado da query pela session
 $_SESSION["queryStatus"] = $response;
 header("Location: ./index.php");
-die();
+exit();
 ?>
