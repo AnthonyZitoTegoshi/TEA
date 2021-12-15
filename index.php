6b10f22af3fc3cfa44d7e18b4c28ff1a99cdbf71
Anthony Zito Tegoshi <zito.a@aluno.ifsp.edu.br>
2021-12-15 17:12:32 -0300

    asd

--- index.php
+++ index.php
@@ -468,14 +468,12 @@
         </div>
     </section>
 
-    <div class="container fit-child screen-center bg-dark-gradient dark padding-5 radius-15 lift-10">
+    <div class="container nunito fit-child screen-center bg-dark-gradient dark padding-5 radius-15 lift-10">
         <div class="row-center items-end flex-nowrap flex-column padding-20 radius-10 bg-light">
-            <div class="col padding-20">
-                Mensagem
+            <div class="col padding-10">
+                Mensagem enviada com sucesso!
             </div>
-            <div class="col padding-20">
-                Mensagem
-            </div>
+            <div class="col padding-10 radius-10 bg-dark-gradient light">Ok!</div>
         </div>
     </div>
 
